30ad597879213a12c37cd28a18fdd42b0e87d1f4
Fawaz Adeniji <fawazadeniji123@gmail.com>
2022-09-03 13:02:31 +0100

    added packege.json

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ async function main() {
 
   const office_quote = await (
     await fetch("https://officeapi.dev/api/quotes/random")
-  ).json(); 
+  ).json();
+
+  console.log(office_quote);
 
   const readme = readmeTemplate
     .replace("{office_quote}", office_quote.data.content)
     .replace(
       "{office_character}",
       `- ${office_quote.data.character.firstname} ${office_quote.data.character.lastname}`
-  );
-  
+    );
+
   await fs.writeFile("README.md", readme);
 }
 
